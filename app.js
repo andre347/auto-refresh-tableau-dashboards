69bcd3eb2766f95953db72be24536026a0bdf4f6
andre347 <aedevries19@gmail.com>
2021-03-29 18:30:07 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+// 1. place to hold the dashboard
+const vizContainer = document.getElementById("vizContainer");
+// 2. the url of the dashboard
+const url =
+  "https://clientreporting.theinformationlab.co.uk/t/PublicDemo/views/PabloTest-Donotdelete/Dashboard1";
+// 3. the options of the dashboard
+const options = {
+  height: 1000,
+  width: 1300,
+  hideToolbar: true,
+};
+let viz;
+let time = 60000;
+let interval;
+
+// refresh time button and input
+const refreshTimeButton = document.getElementById("refreshTimeButton");
+const start = document.getElementById("start");
+const stop = document.getElementById("stop");
+
+refreshTimeButton.addEventListener("click", () => {
+  clearInterval(interval);
+  const refreshTime = document.getElementById("refreshTime").value;
+  time = refreshTime;
+  console.log("setting refresh time", time);
+});
+
+// when the page loads we want to see the dashboard
+function initViz() {
+  viz = new tableau.Viz(vizContainer, url, options);
+}
+
+start.addEventListener("click", () => {
+  // change the status
+  document.getElementById("status").innerHTML = `Refreshing every ${time} ms`;
+  // auto-refresh dashboard
+  interval = setInterval(() => {
+    const currentCount = document.getElementById("count").innerText;
+    console.log(currentCount);
+    const newCount = Number(currentCount) + 1;
+    document.getElementById("count").innerText = newCount;
+
+    const date = Date.now();
+    console.log(`Refreshing every ${time}`);
+    viz.refreshDataAsync();
+  }, time);
+});
+
+stop.addEventListener("click", () => {
+  // change the status
+  document.getElementById("status").innerHTML = `Not refreshing`;
+  clearInterval(interval);
+});
+
+document.addEventListener("DOMContentLoaded", initViz());
